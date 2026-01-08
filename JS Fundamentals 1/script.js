2ff49fbd5f2f8f43918a2f31930992f3d99bd382
lokpalkoranga <suspendedlokpal17@gmail.com>
2026-01-08 12:21:25 +0530

    Scrict Mode

---
+++ JS Fundamentals 1/script.js
@@ -0,0 +1,1 @@
+const console.log(2+3);
---
+++ JS Fundamentals 2/Use Scrict Mode/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let hasDriversLicence = false;
+const passTest = true;
+
+//if (passTest) hasDriverLicence = true;
+
+// there is a differenc of S and strict mode help to find bugs in JS Code
+// (this is the example for strict mode)
+
+if (passTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log("i can drive");
